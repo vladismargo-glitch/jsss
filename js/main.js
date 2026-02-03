b03068ccd9b456c6b9a8ef0a37a1b191caa0e51e
gr434_ivval <vladismargo@gmail.com>
2026-02-03 15:40:58 +0700

    Practical 2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "A pair of warm, fuzzy socks."
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+        description: "A pair of warm, fuzzy socks.",
+        productclasq: "More products like this.",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+
     }
 })
 
