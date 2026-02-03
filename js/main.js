ff36f95e87356baa97f8476d46eee3671777f0e1
gr434_ivval <vladismargo@gmail.com>
2026-02-03 18:16:29 +0700

    Practical 4

--- js/main.js
+++ js/main.js
@@ -9,7 +9,19 @@ let app = new Vue({
         productclasq: "More products like this.",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inventory: 100,
-        onSale: true
+        onSale: false,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
 
     }
 })
