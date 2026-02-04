ab2004f74766e11aff07299b22b49e18619dac98
gr434_ivval <vladismargo@gmail.com>
2026-02-04 19:25:18 +0700

    Practical 7

--- js/main.js
+++ js/main.js
@@ -1,38 +1,62 @@
 let app = new Vue({
     el: '#app',
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+
+        sale() {
+            if (this.onSale) {return this.brand + ' ' + this.product + ' is on sale!';
+
+            }
+            return this.brand + ' ' + this.product + ' is not on sale.';
+        },
+
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+    },
     methods: {
         addToCart() {
             this.cart += 1
         },
         removeFromCart() {
             this.cart -= 1
-        }
+
+        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        },
     },
     data: {
+        brand: "Vue Mastery",
         product: "Socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        selectedVariant: 0,
         altText: "A pair of socks",
         inStock: false,
         description: "A pair of warm, fuzzy socks.",
         productclasq: "More products like this.",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inventory: 0,
-        onSale: false,
-        updateProduct(variantImage) {
-            this.image = variantImage
-        },
-
+        onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
 
@@ -40,6 +64,6 @@ let app = new Vue({
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
     }
-})
 
+})
 
