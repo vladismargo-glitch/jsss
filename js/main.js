4faa4f816b2bf882e275c0fd0797cc07c3f17977
gr434_ivval <vladismargo@gmail.com>
2026-02-05 17:36:42 +0700

    Practical 8

--- js/main.js
+++ js/main.js
@@ -1,69 +1,126 @@
-let app = new Vue({
-    el: '#app',
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product;
-        },
+Vue.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+    `
+})
+Vue.component('product', {
+        props: {
+            premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+    <div class="product">
+        <div class="product-image">
+            <img :src="image" :alt="altText"/>
+        </div>
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p>{{ sale }}</p>
+            <p v-if="inStock > 10">In stock</p>
+            <p v-else-if="inStock <= 10 && inStock > 0">Almost sold out!</p>
+            <p v-else :class="{ outOfStock: !inStock }">Out of stock</p>
+            
+            <ul>
+                <product-details :details="details"></product-details>
+            </ul>
 
-        sale() {
-            if (this.onSale) {return this.brand + ' ' + this.product + ' is on sale!';
+            <div class="color-box"
+                 v-for="(variant, index) in variants"
+                 :key="variant.variantId"
+                 :style="{ backgroundColor: variant.variantColor }"
+                 @mouseover="updateProduct(index)">
+            </div>
 
-            }
-            return this.brand + ' ' + this.product + ' is not on sale.';
-        },
+            <p>Sizes:</p>
+            <ul>
+                <li v-for="size in sizes" :key="size">{{ size }}</li>
+            </ul>
 
-        image() {
-            return this.variants[this.selectedVariant].variantImage;
-        },
+            <button v-on:click="addToCart"
+                    :disabled="!inStock"
+                    :class="{ disabledButton: !inStock }">
+                Add to cart
+            </button>
+            <button v-on:click="removeFromCart">Remove from cart</button>
 
-        inStock(){
-            return this.variants[this.selectedVariant].variantQuantity
-        },
+            <div class="cart">
+                <p>Cart({{ cart }})</p>
+            </div>
+        </div>
+    </div>
+    `,
+    data() {
+        return {
+            brand: "Vue Mastery",
+            product: "Socks",
+            selectedVariant: 0,
+            altText: "A pair of socks",
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                    variantQuantity: 0
+                }
+            ],
+            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            cart: 0,
+            onSale: true
+        }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
         removeFromCart() {
-            this.cart -= 1
-
+            if (this.cart > 0) this.cart -= 1
         },
         updateProduct(index) {
-            this.selectedVariant = index;
-            console.log(index);
-        },
+            this.selectedVariant = index
+        }
     },
-    data: {
-        brand: "Vue Mastery",
-        product: "Socks",
-        selectedVariant: 0,
-        altText: "A pair of socks",
-        inStock: false,
-        description: "A pair of warm, fuzzy socks.",
-        productclasq: "More products like this.",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inventory: 0,
-        onSale: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                variantQuantity: 0
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        shipping() {
+            if (this.premium) {
+                return "Free";
             }
-        ],
-
-
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-        cart: 0,
+            return 2.99;
+        },
+        sale() {
+            return this.onSale ? this.brand + ' ' + this.product + ' is on sale!' : this.brand + ' ' + this.product + ' is not on sale.'
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        }
     }
-
 })
 
+let app = new Vue({
+    el: '#app',
+    data: {
+        premium: true,
+    }
+})
